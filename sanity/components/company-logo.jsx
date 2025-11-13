338606e10c922569bb7004b03f2ca591d76ba450
Hotlines™ <connect@hotlines.studio>
2025-11-13 14:07:12 -0700

    move img to netlify path instead of cms

--- sanity/components/company-logo.jsx
+++ sanity/components/company-logo.jsx
@@ -1,6 +1,6 @@
 export const CompanyLogo = () => (
   <img
-    src="https://cdn.sanity.io/images/mkki3rln/production/c7d3aee0ed2c99fc45f31b31d4dd2b75a8162930-480x480.jpg"
+    src="https://flesh-and-bones.netlify.app/images/sanity-cms-logo-flesh-and-bones.jpg"
     alt='Flesh and Bones'
     width={25}
     height={25}
