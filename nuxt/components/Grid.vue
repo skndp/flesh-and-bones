5303eb4f442e580441572ad4bbc8e81f76fef6ce
Matt Wiggins <matt@consumeandcreate.co>
2025-12-04 11:59:54 -0700

    Moving grid item styles to grid item.

--- nuxt/components/Grid.vue
+++ nuxt/components/Grid.vue
@@ -91,34 +91,6 @@ section.grid {
         &:not(:last-child) {
           margin-bottom: span(1);
         }
-
-        .item {
-          position: relative;
-          aspect-ratio: 16/9;
-          box-shadow: inset 0px 0px 0px 1px $flesh;
-          padding: $space-16;
-          text-align: center;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-direction: column;
-
-          &:nth-child(2) {
-            margin-top: span(1);
-          }
-
-          .meta {
-            width: calc(100% - span(0.5));
-            max-width: 400px;
-            margin: 1em auto 0;
-
-            p {
-              &:nth-child(2) {
-                margin-top: 1em;
-              }
-            }
-          }
-        }
       }
     }
   }
@@ -132,18 +104,6 @@ section.grid {
 
           &:has(.item:only-child) {
             grid-template-columns: 1fr;
-          }
-
-          .item {
-            aspect-ratio: 1/1;
-
-            &:only-child {
-              aspect-ratio: 2/1;
-            }
-
-            &:nth-child(2) {
-              margin-top: 0px;
-            }
           }
         }
       }
@@ -190,30 +150,6 @@ section.grid {
           &:not(:last-child) {
             margin-bottom: span(0.75);
           }
-
-          .item {
-            .meta {
-              text-align: left;
-              display: flex;
-              flex-direction: row;
-              justify-content: space-between;
-
-              &:has(p:only-child) {
-                text-align: center;
-                justify-content: center;
-              }
-
-              p {
-                display: inline-flex;
-
-                &:nth-child(2) {
-                  margin-top: 0px;
-                  margin-left: 1em;
-                  flex-shrink: 0;
-                }
-              }
-            }
-          }
         }
       }
     }
@@ -227,14 +163,6 @@ section.grid {
 
           &:not(:last-child) {
             margin-bottom: span(0.5);
-          }
-
-          .item {
-            aspect-ratio: 4/3;
-
-            &:only-child {
-              aspect-ratio: 8/3;
-            }
           }
         }
       }
--- nuxt/components/GridItemProject.vue
+++ nuxt/components/GridItemProject.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="item flesh">
+  <div class="item project flesh">
     <h3 class="h3">{{ item.title }}</h3>
     <div class="meta">
       <p class="fs-sm"><strong>Director - {{ item.director.title }}</strong></p>
@@ -16,3 +16,77 @@ const props = defineProps({
   }
 });
 </script>
+
+<style lang='scss'>
+.item.project {
+  position: relative;
+  aspect-ratio: 16/9;
+  box-shadow: inset 0px 0px 0px 1px $bone;
+  padding: $space-16;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  &:nth-child(2) {
+    margin-top: span(1);
+  }
+
+  .meta {
+    width: calc(100% - span(0.5));
+    max-width: 400px;
+    margin: 1em auto 0;
+
+    p {
+      &:nth-child(2) {
+        margin-top: 1em;
+      }
+    }
+  }
+
+  @include respond-to($small-tablet) {
+    aspect-ratio: 1/1;
+
+    &:only-child {
+      aspect-ratio: 2/1;
+    }
+
+    &:nth-child(2) {
+      margin-top: 0px;
+    }
+  }
+
+  @include respond-to($tablet) {
+    .meta {
+      text-align: left;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+
+      &:has(p:only-child) {
+        text-align: center;
+        justify-content: center;
+      }
+
+      p {
+        display: inline-flex;
+
+        &:nth-child(2) {
+          margin-top: 0px;
+          margin-left: 1em;
+          flex-shrink: 0;
+        }
+      }
+    }
+  }
+
+  @include respond-to($macbook) {
+    aspect-ratio: 4/3;
+
+    &:only-child {
+      aspect-ratio: 8/3;
+    }
+  }
+}
+</style>
