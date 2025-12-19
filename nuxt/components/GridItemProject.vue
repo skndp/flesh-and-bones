095b4f467cc4e5aed7d70b4aad8c2b8b3af9935c
Hotlines™ <connect@hotlines.studio>
2025-12-19 09:31:06 -0700

    remove unneeded img styles

--- nuxt/components/GridItemProject.vue
+++ nuxt/components/GridItemProject.vue
@@ -215,15 +215,6 @@ function reflow() {
           visibility: visible;
         }
       }
-
-      img {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        width: 100%;
-        height: auto;
-        transform: translate(-50%, -50%);
-      }
     }
   }
 
