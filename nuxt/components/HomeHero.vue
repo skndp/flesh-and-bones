c4176b3c91cf8b34895de8fdfc8aff9bf1da619b
Keith Driessen <connect@hotlines.studio>
2025-12-31 12:33:13 -0700

    hero titles as component

--- nuxt/components/HomeHero.vue
+++ nuxt/components/HomeHero.vue
@@ -1,51 +1,10 @@
 <template>
   <section class="home-hero pad-t pad-b">
     <div class="gutter">
-      <h1 class="h1 flesh rough-edges">
-        <template v-for="(line, index) in titleWords" :key="index">
-          <span v-for="(word, wordIndex) in line" :key="wordIndex">
-            {{ word }}
-            <template v-if="index === 0 && wordIndex === 0 && sketches && sketches.sketch1 && titleWords.length === 1">
-              <div
-                class="sketch1-holder"
-                :style="{
-                  'aspect-ratio': `${sketches.sketch1.image.width}/${sketches.sketch1.image.height}`,
-                  'background-image': `url(${sketches.sketch1.image.src})`
-                }"
-              ></div>
-            </template>
-            <template v-if="index === titleWords.length - 1 && wordIndex === line.length - 1 && sketches && sketches.sketch2 && titleWords.length === 1">
-              <div
-                class="sketch2-holder"
-                :style="{
-                  'aspect-ratio': `${sketches.sketch2.image.width}/${sketches.sketch2.image.height}`,
-                  'background-image': `url(${sketches.sketch2.image.src})`
-                }"
-              ></div>
-            </template>
-            <template v-if="index === 0 && wordIndex === 0 && sketches && sketches.sketch1 && titleWords.length > 1">
-              <div
-                class="sketch1-holder"
-                :style="{
-                  'aspect-ratio': `${sketches.sketch1.image.width}/${sketches.sketch1.image.height}`,
-                  'background-image': `url(${sketches.sketch1.image.src})`
-                }"
-              ></div>
-            </template>
-            <template v-if="index === titleWords.length - 1 && wordIndex === line.length - 1 && sketches && sketches.sketch2 && titleWords.length > 1">
-              <div
-                class="sketch2-holder"
-                :style="{
-                  'aspect-ratio': `${sketches.sketch2.image.width}/${sketches.sketch2.image.height}`,
-                  'background-image': `url(${sketches.sketch2.image.src})`
-                }"
-              ></div>
-            </template>
-            <template v-if="wordIndex < line.length - 1"> </template>
-          </span>
-          <template v-if="index < titleWords.length - 1"> <br></template>
-        </template>
-      </h1>
+      <HeroTitle
+        :title="title"
+        :sketches="sketches"
+      />
       <p v-if="copy" class="manic pad-t">{{ copy }}</p>
     </div>
   </section>
@@ -66,11 +25,6 @@ const props = defineProps({
     type: String,
     required: false
   }
-});
-
-const titleWords = computed(() => {
-  const lines = props.title.split('\n');
-  return lines.map(line => line.trim().split(/\s+/));
 });
 </script>
 
@@ -95,37 +49,11 @@ section.home-hero {
     align-items: center;
     justify-content: center;
 
-    .h1 {
-      position: relative;
-
-      span {
-        position: relative;
-      }
-
-      .sketch1-holder {
-        position: absolute;
-        top: 0.1em;
-        left: 1em;
-        width: auto;
-        height: 0.5em;
-        background-repeat: no-repeat;
-        background-size: contain;
-        background-position: 50% 50%;
-        transform: translateX(-50%);
-        opacity: 0.9;
-      }
-
+    .hero-title {
       .sketch2-holder {
-        position: absolute;
         bottom: -0.3em;
-        right: 0px;
-        width: auto;
         height: 2.3em;
-        background-repeat: no-repeat;
-        background-size: contain;
-        background-position: 50% 50%;
         transform: translateX(80%);
-        opacity: 1;
       }
     }
 
@@ -133,16 +61,6 @@ section.home-hero {
       width: 100%;
       max-width: 666px;
       margin: 0 auto 2em;
-    }
-  }
-
-  @include respond-to($tablet) {
-    .gutter {
-      .h1 {
-        .sketch1-holder {
-          top: 0.04em;
-        }
-      }
     }
   }
 }
--- nuxt/components/ManifestoHero.vue
+++ nuxt/components/ManifestoHero.vue
@@ -2,52 +2,11 @@
   <section class="manifesto-hero pad-t">
     <div class="gutter" :class="{ 'tab-b': !endMarkSketch}">
       <p class="fs-lg flesh manic">{{ kicker }}</p>
-      <h1 class="h1 pad-b flesh rough-edges">
-        <template v-for="(line, index) in titleWords" :key="index">
-          <span v-for="(word, wordIndex) in line" :key="wordIndex">
-            {{ word }}
-            <template v-if="index === 0 && wordIndex === 0 && sketches && sketches.sketch1 && titleWords.length === 1">
-              <div
-                class="sketch1-holder"
-                :style="{
-                  'aspect-ratio': `${sketches.sketch1.image.width}/${sketches.sketch1.image.height}`,
-                  'background-image': `url(${sketches.sketch1.image.src})`
-                }"
-              ></div>
-            </template>
-            <template v-if="index === titleWords.length - 1 && wordIndex === line.length - 1 && sketches && sketches.sketch2 && titleWords.length === 1">
-              <div
-                class="sketch2-holder"
-                :style="{
-                  'aspect-ratio': `${sketches.sketch2.image.width}/${sketches.sketch2.image.height}`,
-                  'background-image': `url(${sketches.sketch2.image.src})`
-                }"
-              ></div>
-            </template>
-            <template v-if="index === 0 && wordIndex === 0 && sketches && sketches.sketch1 && titleWords.length > 1">
-              <div
-                class="sketch1-holder"
-                :style="{
-                  'aspect-ratio': `${sketches.sketch1.image.width}/${sketches.sketch1.image.height}`,
-                  'background-image': `url(${sketches.sketch1.image.src})`
-                }"
-              ></div>
-            </template>
-            <template v-if="index === titleWords.length - 1 && wordIndex === line.length - 1 && sketches && sketches.sketch2 && titleWords.length > 1">
-              <div
-                class="sketch2-holder"
-                :style="{
-                  'aspect-ratio': `${sketches.sketch2.image.width}/${sketches.sketch2.image.height}`,
-                  'background-image': `url(${sketches.sketch2.image.src})`
-                }"
-              ></div>
-            </template>
-            <template v-if="wordIndex < line.length - 1"> </template>
-          </span>
-          <template v-if="index < titleWords.length - 1"> <br></template>
-        </template>
-      </h1>
-      <RichTextSketch :copy="copy" appearance="brush" />
+      <HeroTitle
+        :title="title"
+        :sketches="sketches"
+      />
+      <RichTextSketch :copy="copy" appearance="pad-t brush" />
       <p class="h3 pad-t flesh rough-edges-light">{{ endMark }}</p>
       <div v-if="endMarkSketch" class="end-mark-sketch pad-t h1">
         <div
@@ -90,11 +49,6 @@ const props = defineProps({
     required: false
   }
 });
-
-const titleWords = computed(() => {
-  const lines = props.title.split('\n');
-  return lines.map(line => line.trim().split(/\s+/));
-});
 </script>
 
 <style lang='scss'>
@@ -105,39 +59,6 @@ section.manifesto-hero {
 
   .gutter {
     text-align: center;
-
-    .h1 {
-      position: relative;
-
-      span {
-        position: relative;
-      }
-
-      .sketch1-holder {
-        position: absolute;
-        top: 0.1em;
-        left: 1em;
-        width: auto;
-        height: 0.5em;
-        background-repeat: no-repeat;
-        background-size: contain;
-        background-position: 50% 50%;
-        transform: translateX(-50%);
-        opacity: 0.9;
-      }
-
-      .sketch2-holder {
-        position: absolute;
-        bottom: -0.1em;
-        right: -0.5ch;
-        width: auto;
-        height: 1.3em;
-        background-repeat: no-repeat;
-        background-size: contain;
-        background-position: 50% 50%;
-        opacity: 0.96;
-      }
-    }
 
     .end-mark-sketch {
       height: 1em;
@@ -159,12 +80,6 @@ section.manifesto-hero {
     .gutter {
       &.tab-b {
         padding-bottom: span(1);
-      }
-
-      .h1 {
-        .sketch1-holder {
-          top: 0.04em;
-        }
       }
 
       .end-mark-sketch {
